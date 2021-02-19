ca712539ae6528925d7cf38c7b1876f4ebeb34cc
alejandracu <75814879+alejandracu@users.noreply.github.com>
2021-02-18 20:17:11 -0600

    Update Profesores.java

--- src/ModeloUsuarios/Profesores.java
+++ src/ModeloUsuarios/Profesores.java
@@ -10,6 +10,12 @@ public class Profesores extends Usuario {
      * Default constructor
      */
     public Profesores() {
+        string titulo, nomina;
+        system.out.println("Ingrese el titulo");
+        titulo=titulo.netxLine();
+        system.out.println("Ingrese la nomina");
+        nomina=nomina.nextLine();
     }
+    
 
 }
