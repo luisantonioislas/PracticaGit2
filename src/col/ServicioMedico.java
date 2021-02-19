1ed800955747cced30cfb665158e7bc4d0bb53e5
luisantonioislas <79168509+luisantonioislas@users.noreply.github.com>
2021-02-18 20:11:50 -0600

    ok

---
+++ src/col/ServicioMedico.java
@@ -0,0 +1,57 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class ServicioMedico extends Usuario {
+
+    /**
+     * Default constructor
+     */
+    public ServicioMedico() {
+    }
+
+    /**
+     * 
+     */
+    public String nombrePaciente;
+
+    /**
+     * 
+     */
+    public String matricula;
+
+    /**
+     * 
+     */
+    public int Edad;
+
+    /**
+     * 
+     */
+    public String areaPaciente;
+
+    /**
+     * 
+     */
+    public String motivoConsulta;
+
+    /**
+     * 
+     */
+    public String fechaConsulta;
+
+    /**
+     * 
+     */
+    public String medicamentoRecetado;
+
+    /**
+     * 
+     */
+    public void Operation1() {
+        // TODO implement here
+    }
+
+}
