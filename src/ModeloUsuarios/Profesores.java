76cc4aeeeb7d42a2efd4259b85753f86a9c1bab8
alejandracu <75814879+alejandracu@users.noreply.github.com>
2021-02-18 20:33:28 -0600

    Update Profesores.java

--- src/ModeloUsuarios/Profesores.java
+++ src/ModeloUsuarios/Profesores.java
@@ -10,11 +10,44 @@ public class Profesores extends Usuario {
      * Default constructor
      */
     public Profesores() {
-        string titulo, nomina;
-        system.out.println("Ingrese el titulo");
-        titulo=titulo.netxLine();
-        system.out.println("Ingrese la nomina");
-        nomina=nomina.nextLine();
+       private String Nomina;
+       private String Tipo;
+        private String Titulo;
+        private String Salario:
+        
+        
+       public String getNomina()
+       {
+           return nomina;
+       }
+        public void setNomina(String nomina)
+        {
+            this.nomina=nomina;
+        }
+         public String getTipo()
+       {
+           return Tipo;
+       }
+        public void setTipo(String Tipo)
+        {
+            this.Tipo=Tipo;
+        }
+         public String getTitulo()
+       {
+           return Titulo;
+       }
+        public void setTitulo(String Titulo)
+        {
+            this.Titulo=Titulo;
+        }
+         public String getSalario()
+       {
+           return Salario;
+       }
+        public void setSalario(String Salario)
+        {
+            this.Salario=Salario;
+        }
     }
     
 
